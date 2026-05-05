16f3a123782a83672df2efed11cd27881a8b7b76
nischaljain <nischaljain2000@gmail.com>
2026-05-05 23:44:15 +0530

    End game when score is 0

--- script.js
+++ script.js
@@ -3,11 +3,19 @@
 // A secret number between 1 and 20 that users will be guessing
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 
+// todo: to be removed
+document.querySelector('.number').textContent = secretNumber;
+
 // Score that users will be starting with
 let userScore = 20;
 document.querySelector('.score').textContent = userScore;
 
 document.querySelector('.check').addEventListener('click', function() {
+
+    if (userScore === 0) {
+        document.querySelector('.message').textContent = 'Game over!'
+        return;
+    }
 
     // Get user's guessed number
     const guess = Number(document.querySelector('.guess').value);
